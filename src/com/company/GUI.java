0c73bca8d734cba9927d7195bc3de0a846165cf1
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-13 20:54:07 -0500

    Add panel button

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -5,15 +5,20 @@ import java.awt.*;
 public class GUI {
     public GUI() {
         JFrame frame = new JFrame();
+        JButton button = new JButton("click");
+
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Banking Application");
         frame.pack();
         frame.setVisible(true);
+
+
 
 
 
