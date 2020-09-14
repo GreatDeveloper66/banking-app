c090a78b0f73f7dc685516c88cf9e7580aa99e8a
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-14 15:49:08 -0500

    Add constructor for Account java class

---
+++ src/com/company/Account.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Account {
+    private int AccountNumber;
+    private String Name;
+    private double Amount;
+
+    public Account(int A, String B, double C) {
+        AccountNumber = A;
+        Name = B;
+        Amount = C;
+    }
+}
